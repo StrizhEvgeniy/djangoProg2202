0e7532c32dd7d2fd9750301fa4e7ed822a590851
StrizhEvgeniy <35158193+StrizhEvgeniy@users.noreply.github.com>
2023-03-01 10:20:21 +1000

    01.03

---
+++ local/catalog/models.py
@@ -0,0 +1,101 @@
+import uuid
+
+from django.db import models
+from django.urls import reverse
+
+
+# Create your models here.
+
+
+class Genre(models.Model):
+
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=100, help_text="Enter author first name")
+    last_name = models.CharField(max_length=100, help_text="Enter author lastname")
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=250, help_text="Enter book title")
+    author = models.ForeignKey(Author, models.SET_NULL,null=True)
+    summary = models.CharField(max_length=1500, help_text="Enter book description")
+    genre = models.ManyToManyField(Genre)
+    language = models.ManyToManyField(Language)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    due_back = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(Book, models.SET_NULL, null=True, help_text="Choose a book")
+
+    LOAN_STATUS = (
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+        ('o', 'On Loan'),
+        ('n', 'Not in stock')
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, default='n', help_text='Availability')
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f"{self.book.title} due back {self.due_back} {self.id}"
+# PascalCase C# camel_case python
+
+
+# class MyModelName(models.Model):
+#     """
+#     A typical class defining a model, derived from the Model class.
+#     """
+#
+#     # Fields
+#     my_field_name = models.CharField(max_length=20, help_text="Enter field documentation")
+#     ...
+#
+#     # Metadata
+#     class Meta:
+#         ordering = ["-my_field_name"]
+#
+#
+#     # Methods
+#     def get_absolute_url(self):
+#          """
+#          Returns the url to access a particular instance of MyModelName.
+#          """
+#          return reverse('model-detail-view', args=[str(self.id)])
+#
+#     def __str__(self):
+#         """
+#         String for representing the MyModelName object (in Admin site etc.)
+#         """
+#         return self.field_name
